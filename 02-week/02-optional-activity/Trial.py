709ad1982b56005ebe70143cfdd920ec7c230611
jpgonzalez436 <jpgonzales-2024b@corhuila.edu.co>
2025-08-20 23:29:23 -0500

    Feat :: firstCode

---
+++ 02-week/02-optional-activity/Trial.py
@@ -0,0 +1,5 @@
+# Primer código en Python y visual studio code
+poblacion = "monde"
+print("Bonjour le " +poblacion + ", ceci est mon premier code Python.")
+
+# Para subir esto a github se usa git status, git add, git commit y git push
