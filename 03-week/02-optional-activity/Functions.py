40571be4676c9a621ed4892dbf2362ffadc584ec
jpgonzalez436 <jpgonzales-2024b@corhuila.edu.co>
2025-08-27 22:30:25 -0500

    feat:: functions-py

--- 03-week/02-optional-activity/Functions.py
+++ 03-week/02-optional-activity/Functions.py
@@ -2,5 +2,7 @@
 def notas(n1,n2,n3,n4):
     suma = n1 + n2 + n3 + n4
     return suma/4
-
+#Con def le decimos a python que vamos a crear una función
+#Con n1... le decimos a python que vamos a recibir 4 parámetros y los inicializamos
+#Con return le decimos a python que nos devuelva un valor
 print("La media de las notas es: ", notas(5,7,8,9))
